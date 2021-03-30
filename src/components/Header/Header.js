8af13b9c8d7712a2aaf6fb3f69979259106fbe00
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-03-30 13:35:29 +0300

    99% хедера готово

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
 // Components
-import Container from '../Container/Container';
+import UserMenu from './UserMenu/UserMenu';
+// Styles
+import './Header.module.scss';
+// Others
+import { routes } from '../../routes/routes';
+import sprite from '../../img/sprite.svg';
 
 const Header = () => {
+  // const isAuthenticated = useSelector(state => Boolen(state.auth.token));
+
   return (
-    <Container>
-      <header>
-        <Link></Link>
-      </header>
-    </Container>
+    <header>
+      <Link to={routes.home} className="logo">
+        <svg width="90" height="31">
+          <use href={sprite + '#icon-logo'}></use>
+        </svg>
+      </Link>
+      {/* {isAuthenticated && <UserMenu />} */}
+      <UserMenu />
+    </header>
   );
 };
 
