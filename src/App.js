303b552b5c1afc33b67e68b5848b6ea2de287389
Anur13 <akrupskyi13@gmail.com>
2021-03-30 12:39:40 +0300

    added styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Container from './components/Container';
+import Summary from './components/Summary/Summary';
 
 export default function App() {
   return (
     <>
-      <Container></Container>
+      <Summary></Summary>
     </>
   );
 }
