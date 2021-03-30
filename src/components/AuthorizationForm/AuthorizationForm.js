9330d81a5ddd038e9b09940ab710857e0135ca74
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-03-30 20:03:18 +0300

    доделал разметку и стили для формы авторизации

--- src/components/AuthorizationForm/AuthorizationForm.js
+++ src/components/AuthorizationForm/AuthorizationForm.js
@@ -1,6 +1,11 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 // Components
 import Container from '../Container/Container';
+// Styles
+import styles from './AuthorizationForm.module.scss';
+// Others
+import sprite from '../../img/sprite.svg';
 
 const AuthorizationForm = () => {
   const [email, setEmail] = useState('');
@@ -9,61 +14,70 @@ const AuthorizationForm = () => {
   const changeEmailValue = event => setEmail(event.target.value);
   const changePasswordValue = event => setPassword(event.target.value);
 
-  // const onSubmit = event => {
-  //   event.preventDefault();
-  // };
+  const onSubmit = event => {
+    event.preventDefault();
 
-  const onLogin = event => {
-    console.log(event.target);
-  };
-
-  const onRegistration = event => {
-    console.log(event.target);
+    if (event.nativeEvent.submitter.textContent === 'Войти') {
+      console.log('Запрос для логина');
+    }
+    if (event.nativeEvent.submitter.textContent === 'Регистрация') {
+      console.log('Запрос для регистрации');
+    }
   };
 
   return (
     <Container>
-      <form className="AuthorizationForm" /*onSubmit={onSubmit}*/>
-        <p>Вы можете авторизоваться с помощью Google Account:</p>
-        <button type="button">Google</button>
-        <p>
-          Или зайти с помощью e-mail и пароля, предварительно
-          зарегистрировавшись:
-        </p>
-        <label htmlFor="AuthorizationForm__email">Электронная почта:</label>
-        <input
-          type="email"
-          name="email"
-          id="AuthorizationForm__email"
-          value={email}
-          onChange={changeEmailValue}
-          required
-        />
-        <label htmlFor="AuthorizationForm__password">Пароль:</label>
-        <input
-          type="password"
-          name="password"
-          id="AuthorizationForm__password"
-          value={password}
-          onChange={changePasswordValue}
-          required
-        />
-        <button type="button" className="login-button" onClick={onLogin}>
-          Войти
-        </button>
-        <button
-          type="button"
-          className="registration-button"
-          onClick={onRegistration}
-        >
-          Регистрация
-        </button>
-        {/* <button type="submit" className="login-button">
-        Войти
-      </button>
-      <button type="submit" className="registration-button">
-        Регистрация
-      </button> */}
+      <form className={styles.AuthorizationForm} onSubmit={onSubmit}>
+        <div className={styles.googleAuthBlock}>
+          <p className={styles.googleAuthText}>
+            Вы можете авторизоваться с помощью Google Account:
+          </p>
+          <Link className={styles.googleAuthButton}>
+            <svg width="18" height="18">
+              <use href={sprite + '#icon-google-symbol-1'}></use>
+            </svg>
+            Google
+          </Link>
+        </div>
+        <div className={styles.authBlock}>
+          <p className={styles.authText}>
+            Или зайти с помощью e-mail и пароля, предварительно
+            зарегистрировавшись:
+          </p>
+          <div className={styles.emailBlock}>
+            <label htmlFor="AuthorizationForm__email">Электронная почта:</label>
+            <input
+              type="email"
+              name="email"
+              id="AuthorizationForm__email"
+              value={email}
+              onChange={changeEmailValue}
+              placeholder="your@email.com"
+              required
+            />
+          </div>
+          <div className={styles.passwordBlock}>
+            <label htmlFor="AuthorizationForm__password">Пароль:</label>
+            <input
+              type="password"
+              name="password"
+              id="AuthorizationForm__password"
+              value={password}
+              onChange={changePasswordValue}
+              placeholder="Пароль"
+              minLength="4"
+              required
+            />
+          </div>
+        </div>
+        <div className={styles.buttonsBlock}>
+          <button type="submit" className={styles.loginButton}>
+            Войти
+          </button>
+          <button type="submit" className={styles.registrationButton}>
+            Регистрация
+          </button>
+        </div>
       </form>
     </Container>
   );
