c1be73ca3a5eadc5c2a2064b452283299482ecaa
yankyrychenko <68908555+yankyrychenko@users.noreply.github.com>
2021-03-30 14:07:41 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { Route, Switch } from 'react-router-dom';
 // Components
 import Header from './components/Header/Header';
-import AuthorizationForm from './components/AuthorizationForm/AuthorizationForm';
+import { routes } from './routes/routes';
+
+const AuthorizationView = lazy(() =>
+  import(
+    './pages/AuthorizationView' /* webpackChunkName: "AuthorizationView" */
+  ),
+);
+
+const ExpenseView = lazy(() =>
+  import('./pages/ExpenseView' /* webpackChunkName: "ExpenseView" */),
+);
+
+const IncomeView = lazy(() =>
+  import('./pages/IncomeView' /* webpackChunkName: "IncomeView" */),
+);
+
+const StatisticsView = lazy(() =>
+  import('./pages/StatiscticsView' /* webpackChunkName: "StatisticsView" */),
+);
 
 export default function App() {
   return (
     <>
       <Header />
-      <AuthorizationForm />
+
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Switch>
+          <Route exact path="/" component={AuthorizationView} />
+          <Route exact path={routes.expense} component={ExpenseView} />
+          <Route exact path={routes.income} component={IncomeView} />
+          <Route exact path={routes.stats} component={StatisticsView} />
+        </Switch>
+      </Suspense>
     </>
   );
 }
