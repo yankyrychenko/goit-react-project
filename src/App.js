0fa38c43d449568056b4ce3f1765ebe6caf99435
Anur13 <akrupskyi13@gmail.com>
2021-03-30 14:39:31 +0300

    added month change

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import Container from './components/Container';
 import Header from './components/Header/Header';
 import AuthorizationForm from './components/AuthorizationForm/AuthorizationForm';
+import Summary from './components/Summary/Summary';
+import MonthArrows from './components/MonthArrows/MonthArrows';
 
 export default function App() {
   return (
     <>
       <Header />
       <AuthorizationForm />
+      <MonthArrows />
     </>
   );
 }
