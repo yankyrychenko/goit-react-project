e2cd14b01aa936d7a5380269d1f54596045a458d
AndriiSorokin <andrewsorokin07@gmail.com>
2021-03-30 17:38:05 +0300

    add modal balance

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import GoToReport  from './components/GoToReport/GoToReport'
 import Container from './components/Container';
 import BalanceCustom from './components/BalanceCustom/BalanceCustom';
+import BalanceModal from './components/BalanceModal/BalanceModal';
 
 export default function App() {
   return (
     <>
       <Container>
         <GoToReport />
-        <BalanceCustom/>
+        <BalanceCustom />
+        <BalanceModal/>
       </Container>
     </>
   );
---
+++ src/components/BalanceModal/BalanceModal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './BalanceModal.module.scss'
+const BalanceModal = () => {
+  return (
+    <div className={style.balanceModal}>
+      <div className={style.triangleUp}></div>
+      <p className={style.text}>Привет! Для начала работы внеси текущий баланс своего счета!</p>
+      <p className={style.text}>Ты не можешь тратить деньги пока
+их у тебя нет :)</p>
+    </div>
+  );
+};
+
+export default BalanceModal;
