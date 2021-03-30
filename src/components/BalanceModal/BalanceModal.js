3b7236db047ab5e0d6e782e24161fc785dcc989e
AndriiSorokin <andrewsorokin07@gmail.com>
2021-03-30 18:00:44 +0300

    make triangle before element

--- src/components/BalanceModal/BalanceModal.js
+++ src/components/BalanceModal/BalanceModal.js
@@ -3,7 +3,6 @@ import style from './BalanceModal.module.scss'
 const BalanceModal = () => {
   return (
     <div className={style.balanceModal}>
-      <div className={style.triangleUp}></div>
       <p className={style.text}>Привет! Для начала работы внеси текущий баланс своего счета!</p>
       <p className={style.text}>Ты не можешь тратить деньги пока
 их у тебя нет :)</p>
