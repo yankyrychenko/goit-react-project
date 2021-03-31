14ffdab7fe075d7e3fe6567f300823e28e1fe854
AndriiSorokin <andrewsorokin07@gmail.com>
2021-03-31 12:28:13 +0300

    style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import GoToReport  from './components/GoToReport/GoToReport'
 import Container from './components/Container';
-import BalanceCustom from './components/BalanceCustom/BalanceCustom';
 
 export default function App() {
   return (
     <>
       <Container>
-        <GoToReport />
-        <BalanceCustom />
       </Container>
     </>
   );
