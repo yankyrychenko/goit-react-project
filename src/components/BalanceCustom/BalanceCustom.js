3aaf6768366c2345f03c6bcfa67d1f053f2e4eff
AndriiSorokin <andrewsorokin07@gmail.com>
2021-03-31 14:43:09 +0300

    make logic

--- src/components/BalanceCustom/BalanceCustom.js
+++ src/components/BalanceCustom/BalanceCustom.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './BalanceCustom.module.scss'
 import BalanceModal from '../BalanceModal/BalanceModal'
 
+const initialState = {
+  balance: ''
+}
+
 const BalanceCustom = () => {
+  const [balance, setBalance] = useState({...initialState})
+
+  const balanceHandler = ({ target }) => {
+    const {name, value } = target
+    setBalance(state =>({...state, [name]:value}))
+  }
+
+  const balanceSubmit = e => {
+    e.preventDefault()
+
+  }
+  console.log(balance);
   return (
     <div className={style.balanceWrapper} >
       <p className={style.balanceText}>Баланс:</p>
-      <form className={style.balanceForm}>
-        <input className={style.balanceInput} type="number" name="blance" placeholder=" 00.00 UAH" />
+      <form className={style.balanceForm} onSubmit={balanceSubmit}>
+        <input onChange={balanceHandler} className={style.balanceInput} type="number" name="balance" placeholder=" 00.00 UAH" value={balance.balance} />
         <button className={style.balanceButton}>ПОДТВЕРДИТЬ</button>
-        <BalanceModal/>
+        {balance.balance > 0 ? '' : <BalanceModal/> }
+
       </form>
     </div>
   );
