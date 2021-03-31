5cbc0710f44d26d8dbd1e67cfbaa5c4a0b4e69cc
yankyrychenko <68908555+yankyrychenko@users.noreply.github.com>
2021-03-31 13:09:35 +0300

    add errors

--- src/redux/actions/periodDataActions.js
+++ src/redux/actions/periodDataActions.js
@@ -4,6 +4,7 @@ const periodDataGetRequest = createAction('auth/periodDataGetRequest');
 const periodDataGetSuccess = createAction('auth/periodDataGetSuccess');
 const periodDataGetError = createAction('auth/periodDataGetError');
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   periodDataGetRequest,
   periodDataGetSuccess,
