3ca9be6a62ca0df90c16e2103cf0a4161072157d
yankyrychenko <68908555+yankyrychenko@users.noreply.github.com>
2021-03-31 12:13:47 +0300

    pages fixes

--- src/pages/IncomeView/IncomeView.js
+++ src/pages/IncomeView/IncomeView.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Container from '../../components/Container';
 
-const IncomeView = () => <></>;
+const IncomeView = () => (
+  <>
+    <main>
+      <Container></Container>
+    </main>
+  </>
+);
 
 export default IncomeView;
