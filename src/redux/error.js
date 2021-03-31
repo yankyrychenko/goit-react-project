7b3d2de0d6bd2efb4e6b3ef1ee9330cc088fe0d7
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-03-31 17:34:01 +0300

    написал errorReducer

--- src/redux/error.js
+++ src/redux/error.js
@@ -1,7 +1,17 @@
 import { createReducer } from '@reduxjs/toolkit';
+import authActions from './actions/authActions';
 
 const intialState = null;
+const errorReducer = createReducer(intialState, {
+  [authActions.signUpError]: (_, { payload }) => payload,
+  [authActions.logInError]: (_, { payload }) => payload,
+  [authActions.logOutError]: (_, { payload }) => payload,
+  [authActions.getCurrentUserError]: (_, { payload }) => payload,
 
-const errorReducer = createReducer(intialState, {});
+  [authActions.signUpRequest]: () => intialState,
+  [authActions.logInRequest]: () => intialState,
+  [authActions.logOutRequest]: () => intialState,
+  [authActions.getCurrentUserRequest]: () => intialState,
+});
 
 export default errorReducer;
