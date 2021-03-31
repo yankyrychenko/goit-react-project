b75732d6d0d8fdc5eee3d85f8a00473470d7b997
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2021-03-31 20:30:35 +0300

    added format currency

--- src/components/StatementBar/StatementBar.js
+++ src/components/StatementBar/StatementBar.js
@@ -7,6 +7,16 @@ import {
 } from '../../redux/selectors/periodDataSelectors';
 import styles from './StatementBar.module.scss';
 
+Number.prototype.format = function (n, x, s, c) {
+  var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+    num = this.toFixed(Math.max(0, ~~n));
+
+  return (c ? num.replace('.', c) : num).replace(
+    new RegExp(re, 'g'),
+    '$&' + (s || ','),
+  );
+};
+
 const StatementBar = () => {
   const dispatch = useDispatch();
 
@@ -14,12 +24,16 @@ const StatementBar = () => {
   useEffect(() => {
     setTimeout(() => {
       dispatch(periodDate.getPeriodData('2020-04'));
-    }, 6000);
+    }, 3000);
   }, []);
 
   const incomeTotal = useSelector(getIncomeTotal);
 
   const expenseTotal = useSelector(getExpenseTotal);
+
+  const valueIncomeTotal = incomeTotal.format(2, 3, ' ', '.');
+
+  const valueExpenseTotal = expenseTotal.format(2, 3, ' ', '.');
 
   return (
     <div className={styles.wrap}>
@@ -28,7 +42,7 @@ const StatementBar = () => {
           <div className={styles.containerDetails}>
             <h2 className={styles.statementBarTitle}>Расходы:</h2>
             <div className={styles.expensesAmount}>
-              &#45; {expenseTotal} грн.
+              &#45; {valueExpenseTotal} грн.
             </div>
           </div>
         </li>
@@ -37,7 +51,9 @@ const StatementBar = () => {
         >
           <div className={styles.containerDetails}>
             <h2 className={styles.statementBarTitle}>Доходы:</h2>
-            <div className={styles.incomeAmount}>&#43; {incomeTotal} грн.</div>
+            <div className={styles.incomeAmount}>
+              &#43; {valueIncomeTotal} грн.
+            </div>
           </div>
         </li>
       </ul>
