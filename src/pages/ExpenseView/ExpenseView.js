7976e7b28f6cc2cefa19311519a22eec4193d98f
yankyrychenko <68908555+yankyrychenko@users.noreply.github.com>
2021-04-01 12:57:40 +0300

    add components

--- src/pages/ExpenseView/ExpenseView.js
+++ src/pages/ExpenseView/ExpenseView.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import Container from '../../components/Container';
-// import PagesCost from './../../components/PagesCost/CostPage';
+import TransactionContainer from '../../components/TransactionContainer/TransactionContainer';
+import BalanceForm from '../../components/BalanceForm';
+import TransactionTable from '../../components/TransactionTable/TransactionTable';
+import Summary from '../../components/Summary/Summary';
 import transactionsOperations from '../../redux/operations/transactionsOperations';
 import categoriesOperations from '../../redux/operations/categoriesOperations';
+import style from './ExpenseView.module.scss';
 
 export default function ExpenseView() {
   const dispatch = useDispatch();
@@ -16,7 +20,13 @@ export default function ExpenseView() {
   return (
     <main>
       <Container>
-        {/* <PagesCost /> */}
+        <TransactionContainer>
+          <BalanceForm />
+          <div className={style.wrapper}>
+            <TransactionTable />
+            <Summary />
+          </div>
+        </TransactionContainer>
       </Container>
     </main>
   );
