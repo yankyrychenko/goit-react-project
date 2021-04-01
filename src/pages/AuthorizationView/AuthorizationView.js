386f6e210216accf6a6d96d132200705b8772f2f
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2021-04-01 11:08:14 +0300

    fixed stayles

--- src/pages/AuthorizationView/AuthorizationView.js
+++ src/pages/AuthorizationView/AuthorizationView.js
@@ -3,16 +3,13 @@ import Container from '../../components/Container';
 import PageTitle from '../../components/PageTitle';
 import AuthorizationForm from '../../components/AuthorizationForm/AuthorizationForm';
 import styles from './AuthorizationView.module.scss';
-import Modal from '../../components/Modal/Modal';
 
 const AuthorizationView = () => (
   <main>
     <Container>
       <div className={styles.authContainer}>
         <PageTitle />
-        <Modal>
-          <AuthorizationForm />
-        </Modal>
+        <AuthorizationForm />
       </div>
     </Container>
   </main>
