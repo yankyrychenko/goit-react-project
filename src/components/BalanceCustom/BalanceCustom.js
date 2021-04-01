7fdfb2fec14261703cd808046f5d29edb892adaa
AndriiSorokin <andrewsorokin07@gmail.com>
2021-04-01 13:21:54 +0300

    merge

--- src/components/BalanceCustom/BalanceCustom.js
+++ src/components/BalanceCustom/BalanceCustom.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+// import {useDispatch} from 'react-redux'
 import style from './BalanceCustom.module.scss'
-import animation from './BalanceAnimation.module.scss'
 import BalanceModal from '../BalanceModal/BalanceModal'
-import {CSSTransition} from 'react-transition-group'
 
 const initialState = {
   balance: ''
@@ -15,23 +14,17 @@ const BalanceCustom = () => {
     const {name, value } = target
     setBalance(state =>({...state, [name]:value}))
   }
-
   const balanceSubmit = e => {
     e.preventDefault()
   }
 
-
-  console.log(balance);
   return (
     <div className={style.balanceWrapper} >
       <p className={style.balanceText}>Баланс:</p>
       <form className={style.balanceForm} onSubmit={balanceSubmit}>
         <input onChange={balanceHandler} className={style.balanceInput} type="number" name="balance" placeholder=" 00.00 UAH" value={balance.balance} />
         <button className={style.balanceButton}>ПОДТВЕРДИТЬ</button>
-        {balance.balance > 0 ? '' :
-          <CSSTransition in={true} classNames={animation} timeout={1000} unmountOnExit>
-            <BalanceModal />
-          </CSSTransition>
+        {balance.balance > 0 ? '' : <BalanceModal />
         }
       </form>
     </div>
