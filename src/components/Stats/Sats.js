2c1eec22baff25253d0ef0d957fb34908b4fd7c0
Aleksey Kolpakov <gmsaleksei@gmail.com>
2021-04-01 12:17:55 +0300

    responsive+shapes

--- src/components/Stats/Sats.js
+++ src/components/Stats/Sats.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   BarChart,
   Bar,
@@ -7,6 +7,8 @@ import {
   YAxis,
   CartesianGrid,
   ResponsiveContainer,
+  Rectangle,
+  LabelList,
 } from 'recharts';
 const data = [
   {
@@ -20,7 +22,6 @@ const data = [
   {
     name: 'Курятина',
     uv: 2000,
-
   },
   {
     name: 'Шоколад',
@@ -45,32 +46,55 @@ const data = [
   },
 ];
 const Stats = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleWindowResize = () => {
+      if (window.innerWidth <= 768) {
+        setIsMobile(true);
+        return;
+      }
+      setIsMobile(false);
+    };
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
   return (
     <>
       <ResponsiveContainer width="100%" height="100%" minHeight={422}>
         <BarChart
+          barCategoryGap="20%"
           height={422}
           data={data}
+          layout={isMobile ? 'vertical' : 'horizontal'}
           margin={{
             top: 20,
             right: 30,
             left: 20,
             bottom: 5,
           }}
-          //   layout="vertical"
         >
+         
+          {isMobile ? <XAxis type="number" hide /> : <XAxis dataKey="name" axisLine={false} tickLine={false} />}
+          {isMobile ? <YAxis type="category" hide dataKey="name" /> : <YAxis type="number" axisLine={false} tick={true} />}
           {/* <XAxis type="number" />
           <YAxis type="category" dataKey="name" /> */}
-          <XAxis dataKey="name" />
+          
+          {!isMobile &&<CartesianGrid vertical={false} strokeDashArray="4" />}
           <Bar
+            maxBarSize={38}
+            shape={
+              <Rectangle radius={isMobile ? [0, 10, 10, 0] : [10, 10, 0, 0]} />
+            }
             dataKey="uv"
             fill="#8884d8"
             // shape={<TriangleBar />}
-            label={{ position: 'top' }}
+            label={{ position: 'top', fill: '#FF9900' }}
           >
+            
             {data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill="#FF9900" />
             ))}
+            {/* <LabelList dataKey="name" position="bottom" /> */}
           </Bar>
         </BarChart>
       </ResponsiveContainer>
