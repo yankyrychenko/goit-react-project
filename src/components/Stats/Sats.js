e95031c1c63e60b4ef3796017e28d2a959d19f06
Aleksey Kolpakov <gmsaleksei@gmail.com>
2021-04-01 14:23:40 +0300

    connect to redux

--- src/components/Stats/Sats.js
+++ src/components/Stats/Sats.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import {
   BarChart,
   Bar,
@@ -10,43 +11,25 @@ import {
   Rectangle,
   LabelList,
 } from 'recharts';
-const data = [
-  {
-    name: 'Свинина',
-    uv: 4000,
-  },
-  {
-    name: 'Говядина',
-    uv: 3000,
-  },
-  {
-    name: 'Курятина',
-    uv: 2000,
-  },
-  {
-    name: 'Шоколад',
-    uv: 2780,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-  },
-  {
-    name: 'Свинина',
-    uv: 4000,
-  },
-];
-const Stats = () => {
+import { getPeriodData } from '../../redux/selectors/periodDataSelectors';
+
+const Stats = ({ category }) => {
+  const getDataForDiagram = (category, periodData) => {
+    const categoryData = periodData?.incomes?.incomesData[category]
+      ? periodData.incomes.incomesData[category]
+      : periodData?.expenses?.incomesData[category];
+    const categoryDataKeys = Object.keys(categoryData).slice(1);
+    const formatedCategoryData = categoryDataKeys.map(category => ({
+      name: category,
+      data: categoryData[category],
+    }));
+    return formatedCategoryData.sort((a,b)=>b.data-a.data);
+  };
+
   const [isMobile, setIsMobile] = useState(false);
+  const periodData = useSelector(getPeriodData);
+  console.log(periodData);
+  const diagramData = getDataForDiagram('Транспорт', periodData);
   useEffect(() => {
     const handleWindowResize = () => {
       if (window.innerWidth <= 768) {
@@ -64,7 +47,7 @@ const Stats = () => {
         <BarChart
           barCategoryGap="20%"
           height={422}
-          data={data}
+          data={diagramData}
           layout={isMobile ? 'vertical' : 'horizontal'}
           margin={{
             top: 20,
@@ -73,25 +56,31 @@ const Stats = () => {
             bottom: 5,
           }}
         >
-         
-          {isMobile ? <XAxis type="number" hide /> : <XAxis dataKey="name" axisLine={false} tickLine={false} />}
-          {isMobile ? <YAxis type="category" hide dataKey="name" /> : <YAxis type="number" axisLine={false} tick={true} />}
+          {isMobile ? (
+            <XAxis type="number" hide />
+          ) : (
+            <XAxis dataKey="name" axisLine={false} tickLine={false} />
+          )}
+          {isMobile ? (
+            <YAxis type="category" hide dataKey="name" />
+          ) : (
+            <YAxis type="number" axisLine={false} tick={true} />
+          )}
           {/* <XAxis type="number" />
           <YAxis type="category" dataKey="name" /> */}
-          
-          {!isMobile &&<CartesianGrid vertical={false} strokeDashArray="4" />}
+
+          {!isMobile && <CartesianGrid vertical={false} strokeDashArray="4" />}
           <Bar
             maxBarSize={38}
             shape={
               <Rectangle radius={isMobile ? [0, 10, 10, 0] : [10, 10, 0, 0]} />
             }
-            dataKey="uv"
+            dataKey="data"
             fill="#8884d8"
             // shape={<TriangleBar />}
             label={{ position: 'top', fill: '#FF9900' }}
           >
-            
-            {data.map((entry, index) => (
+            {diagramData.map((entry, index) => (
               <Cell key={`cell-${index}`} fill="#FF9900" />
             ))}
             {/* <LabelList dataKey="name" position="bottom" /> */}
