11d6e8133a1daa510fbd2ae8be3c4b7e4192a432
Anur13 <akrupskyi13@gmail.com>
2021-04-01 13:07:10 +0300

    mid-way

--- src/pages/ExpenseView/ExpenseView.js
+++ src/pages/ExpenseView/ExpenseView.js
@@ -4,6 +4,7 @@ import Container from '../../components/Container';
 import PagesCost from './../../components/PagesCost/CostPage';
 import transactionsOperations from '../../redux/operations/transactionsOperations';
 import categoriesOperations from '../../redux/operations/categoriesOperations';
+import Summary from '../../components/Summary/Summary';
 
 export default function ExpenseView() {
   const dispatch = useDispatch();
--- src/pages/StatiscticsView/StatiscticsView.js
+++ src/pages/StatiscticsView/StatiscticsView.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import Stats from '../../components/Stats/Sats';
 import Container from '../../components/Container';
 import periodDataOperations from '../../redux/operations/periodDataOperations';
+import MonthCalendar from '../../components/MonthCalendar/MonthCalendar';
 
 export default function StatisticsView() {
   /*   const dispatch = useDispatch();
@@ -14,6 +15,8 @@ export default function StatisticsView() {
   return (
     <main>
       <Container>
+        <MonthCalendar />
+
         <Stats />
       </Container>
     </main>
