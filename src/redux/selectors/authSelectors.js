61509d4419ebc05be4a39da3fbd0165d6ab65db6
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-04-01 19:01:15 +0300

    добавил селектор на токен

--- src/redux/selectors/authSelectors.js
+++ src/redux/selectors/authSelectors.js
@@ -1,2 +1,3 @@
 export const getUserEmail = state => state.auth.user.email;
 export const getIsAuthenticated = state => state.auth.isAuthenticated;
+export const getToken = state => state.auth.token;
