cbd3ae33b11a745d8bcff3415d06a460d7d94588
Eduard Batiushchenko <72995778+Eduard-Batiushchenko@users.noreply.github.com>
2021-04-01 16:47:11 +0300

    almost done expenses and icomes

--- src/pages/ExpenseView/ExpenseView.js
+++ src/pages/ExpenseView/ExpenseView.js
@@ -1,21 +1,30 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Container from '../../components/Container';
 import PagesCost from './../../components/PagesCost/CostPage';
 import transactionsOperations from '../../redux/operations/transactionsOperations';
 import categoriesOperations from '../../redux/operations/categoriesOperations';
+import BalanceForm from '../../components/BalanceForm/BalanceForm';
+import { getCategoryExpense } from '../../redux/selectors/categoriesSelectors';
 
 export default function ExpenseView() {
   const dispatch = useDispatch();
+  const category = useSelector(getCategoryExpense);
 
   useEffect(() => {
     dispatch(transactionsOperations.handleExpenseGet());
     dispatch(categoriesOperations.handleExpenseCategGet());
   }, [dispatch]);
 
+  const submitIncomeData = data => {
+    const finalData = { ...data, amount: Number(data.amount) };
+    dispatch(transactionsOperations.handleExpensePost(finalData));
+  };
+
   return (
     <main>
       <Container>
+        <BalanceForm category={category} submitIncomeData={submitIncomeData} />
         <PagesCost />
       </Container>
     </main>
--- src/pages/IncomeView/IncomeView.js
+++ src/pages/IncomeView/IncomeView.js
@@ -1,21 +1,30 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Container from '../../components/Container';
 import PagesCost from './../../components/PagesCost/CostPage';
 import transactionsOperations from '../../redux/operations/transactionsOperations';
 import categoriesOperations from '../../redux/operations/categoriesOperations';
+import BalanceForm from '../../components/BalanceForm/BalanceForm';
+import { getCategoryIncome } from '../../redux/selectors/categoriesSelectors';
 
 export default function IncomeView() {
   const dispatch = useDispatch();
+  const category = useSelector(getCategoryIncome);
 
   useEffect(() => {
     dispatch(transactionsOperations.handleIncomeGet());
     dispatch(categoriesOperations.handleIncomeCategGet());
   }, [dispatch]);
 
+  const submitIncomeData = data => {
+    const finalData = { ...data, amount: Number(data.amount) };
+    dispatch(transactionsOperations.handleIncomePost(finalData));
+  };
+
   return (
     <main>
       <Container>
+        <BalanceForm category={category} submitIncomeData={submitIncomeData} />
         <PagesCost />
       </Container>
     </main>
