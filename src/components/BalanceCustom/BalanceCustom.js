2c4a229d9d0f6e0316d17cbbf7b401c9313a6c9e
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-04-02 14:52:17 +0300

    подправил стили блока с балансом и Перейти к отчетам

--- src/components/BalanceCustom/BalanceCustom.js
+++ src/components/BalanceCustom/BalanceCustom.js
@@ -1,37 +1,45 @@
 import React, { useState } from 'react';
-import {useSelector, useDispatch} from 'react-redux'
-import style from './BalanceCustom.module.scss'
-import BalanceModal from '../BalanceModal/BalanceModal'
-import { getUserBalance } from '../../redux/selectors/authSelectors'
-import {addBalance} from '../../redux/operations/balanceOperations'
+import { useSelector, useDispatch } from 'react-redux';
+import style from './BalanceCustom.module.scss';
+import BalanceModal from '../BalanceModal/BalanceModal';
+import { getUserBalance } from '../../redux/selectors/authSelectors';
+import { addBalance } from '../../redux/operations/balanceOperations';
 
 const initialState = {
-  newBalance: ''
-}
+  newBalance: '',
+};
 
 const BalanceCustom = () => {
-  const [newBalance, setNewBalance] = useState({...initialState })
-  const dispatch = useDispatch()
+  const [newBalance, setNewBalance] = useState({ ...initialState });
+  const dispatch = useDispatch();
   const balanceHandler = ({ target }) => {
-    const {name, value } = target
-    setNewBalance(state =>({...state, [name]:value}))
-  }
+    const { name, value } = target;
+    setNewBalance(state => ({ ...state, [name]: value }));
+  };
   const balanceSubmit = e => {
-    e.preventDefault()
+    e.preventDefault();
     console.log(newBalance);
-    dispatch(addBalance(newBalance))
-  }
-  const currentBalance = useSelector(state => getUserBalance(state))
+    dispatch(addBalance(newBalance));
+  };
+  const currentBalance = useSelector(state => getUserBalance(state));
 
   return (
-    <div className={style.balanceWrapper} >
+    <div className={style.balanceWrapper}>
       <p className={style.balanceText}>Баланс:</p>
       <form className={style.balanceForm} onSubmit={balanceSubmit}>
-        <input onChange={balanceHandler} className={style.balanceInput} type="number" name="newBalance"
-          placeholder={currentBalance === 0 ? '00:00 UAH' : currentBalance} value={newBalance.newBalance} />
-        <button className={style.balanceButton}>ПОДТВЕРДИТЬ</button>
-        {newBalance.newBalance && currentBalance == 0 ? <BalanceModal /> : null }
+        <input
+          onChange={balanceHandler}
+          className={style.balanceInput}
+          type="number"
+          name="newBalance"
+          placeholder={currentBalance === 0 ? '00.00 UAH' : currentBalance}
+          value={newBalance.newBalance}
+        />
+        <button type="submit" className={style.balanceButton}>
+          ПОДТВЕРДИТЬ
+        </button>
       </form>
+      {newBalance.newBalance && currentBalance == 0 ? <BalanceModal /> : null}
     </div>
   );
 };
