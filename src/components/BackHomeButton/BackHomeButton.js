b9e27665853bff2560e6011fc6086065b2ec45c8
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-04-02 15:23:37 +0300

    внес мелкие правки

--- src/components/BackHomeButton/BackHomeButton.js
+++ src/components/BackHomeButton/BackHomeButton.js
@@ -5,15 +5,13 @@ import styles from './BackHomeButton.module.scss';
 import sprite from '../../img/sprite.svg';
 import routes from '../../routes/routes';
 
-const BackHomeButton = ({ text }) => {
-  return (
-    <Link to={routes.home} className={styles.BackHomeButton}>
-      <svg width="24" height="24">
-        <use href={sprite + '#icon-back-home'}></use>
-      </svg>
-      {text}
-    </Link>
-  );
-};
+const BackHomeButton = () => (
+  <Link to={routes.home} className={styles.BackHomeButton}>
+    <svg width="24" height="24">
+      <use href={sprite + '#icon-back-home'}></use>
+    </svg>
+    <span>Вернуться на главную</span>
+  </Link>
+);
 
 export default BackHomeButton;
