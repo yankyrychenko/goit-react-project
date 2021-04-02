00c056a3742bd661864e7e093837b9949e4249c2
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-04-02 16:19:10 +0300

    подправил компонент, рендерю текст в зависимости от вьюпорта

--- src/components/BackHomeButton/BackHomeButton.js
+++ src/components/BackHomeButton/BackHomeButton.js
@@ -4,14 +4,19 @@ import styles from './BackHomeButton.module.scss';
 // Others
 import sprite from '../../img/sprite.svg';
 import routes from '../../routes/routes';
+import { useBreakpoint } from 'react-use-size';
 
-const BackHomeButton = () => (
-  <Link to={routes.home} className={styles.BackHomeButton}>
-    <svg width="24" height="24">
-      <use href={sprite + '#icon-back-home'}></use>
-    </svg>
-    <span>Вернуться на главную</span>
-  </Link>
-);
+const BackHomeButton = () => {
+  const mobile = useBreakpoint(768);
+
+  return (
+    <Link to={routes.home} className={styles.BackHomeButton}>
+      <svg width="24" height="24">
+        <use href={sprite + '#icon-back-home'}></use>
+      </svg>
+      {!mobile && <span>Вернуться на главную</span>}
+    </Link>
+  );
+};
 
 export default BackHomeButton;
