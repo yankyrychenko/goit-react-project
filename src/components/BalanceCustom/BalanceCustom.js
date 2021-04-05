a3f8f7e58ac1ea085da10e35f6e99fd2c39c5eab
AndriiSorokin <andrewsorokin07@gmail.com>
2021-04-05 13:11:03 +0300

    fixed bag and add functional

--- src/components/BalanceCustom/BalanceCustom.js
+++ src/components/BalanceCustom/BalanceCustom.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import {useLocation} from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
 import style from './BalanceCustom.module.scss';
 import BalanceModal from '../BalanceModal/BalanceModal';
 import { getUserBalance } from '../../redux/selectors/authSelectors';
 import { addBalance } from '../../redux/operations/balanceOperations';
+import { useWindowSize } from 'react-use-size';
 
 const initialState = {
   newBalance: '',
@@ -11,7 +13,13 @@ const initialState = {
 
 const BalanceCustom = () => {
   const [newBalance, setNewBalance] = useState({ ...initialState });
+  const [read, setRead] = useState(false)
+
+  const currentBalance = useSelector(state => getUserBalance(state));
+  const { width } = useWindowSize();
+  const location = useLocation()
   const dispatch = useDispatch();
+
   const balanceHandler = ({ target }) => {
     const { name, value } = target;
     setNewBalance(state => ({ ...state, [name]: value }));
@@ -21,7 +29,6 @@ const BalanceCustom = () => {
     console.log(newBalance);
     dispatch(addBalance(newBalance));
   };
-  const currentBalance = useSelector(state => getUserBalance(state));
 
   return (
     <div className={style.balanceWrapper}>
@@ -30,6 +37,7 @@ const BalanceCustom = () => {
           Баланс:
         </label>
         <input
+          readOnly={currentBalance ? !read : read}
           maxLength="6"
           min="1"
           max="999999"
@@ -41,11 +49,13 @@ const BalanceCustom = () => {
           placeholder={currentBalance === 0 ? '00.00 UAH' : currentBalance}
           value={newBalance.newBalance}
         />
-        <button type="submit" className={style.balanceButton}>
-          ПОДТВЕРДИТЬ
-        </button>
+       {location.pathname === '/statistics' && width < 767 ? null : (
+          <button type="submit" className={style.balanceButton}>
+            ПОДТВЕРДИТЬ
+          </button>
+        )}
+        { currentBalance == 0 && location.pathname !== '/statistics' ?  <BalanceModal/> : null }
       </form>
-      {currentBalance == 0 ? <BalanceModal /> : null}
     </div>
   );
 };
