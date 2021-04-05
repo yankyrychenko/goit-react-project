3b8e9355e720105666f2bafb423e242d94a2a507
Anur13 <akrupskyi13@gmail.com>
2021-04-05 11:39:46 +0300

    fin

--- src/components/Stats/Sats.js
+++ src/components/Stats/Sats.js
@@ -12,6 +12,7 @@ import {
   LabelList,
 } from 'recharts';
 import { getPeriodData } from '../../redux/selectors/periodDataSelectors';
+import styles from './Stats.module.scss';
 
 const Stats = ({ category }) => {
   const getDataForDiagram = (category, periodData) => {
@@ -30,7 +31,7 @@ const Stats = ({ category }) => {
 
   const [isMobile, setIsMobile] = useState(false);
   const periodData = useSelector(getPeriodData);
-  const diagramData = getDataForDiagram((category), periodData);
+  const diagramData = getDataForDiagram(category, periodData);
   useEffect(() => {
     const handleWindowResize = () => {
       if (window.innerWidth <= 768) {
@@ -43,52 +44,64 @@ const Stats = ({ category }) => {
     return () => window.removeEventListener('resize', handleWindowResize);
   }, []);
   return (
-  <> {diagramData&&
-    <ResponsiveContainer width="100%" height="100%" minHeight={422}>
-      <BarChart
-        barCategoryGap="20%"
-        height={422}
-        data={diagramData}
-        layout={isMobile ? 'vertical' : 'horizontal'}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        {isMobile ? (
-          <XAxis type="number" hide />
-        ) : (
-          <XAxis dataKey="name" axisLine={false} tickLine={false} />
-        )}
-        {isMobile ? (
-          <YAxis type="category" hide dataKey="name" />
-        ) : (
-          <YAxis type="number" axisLine={false} tick={true} />
-        )}
-        {/* <XAxis type="number" />
+    <div className={styles.wrapper}>
+      {' '}
+      {diagramData && (
+        <ResponsiveContainer width="100%" height="100%" minHeight={422}>
+          <BarChart
+            barCategoryGap="20%"
+            height={422}
+            data={diagramData}
+            layout={isMobile ? 'vertical' : 'horizontal'}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            {isMobile ? (
+              <XAxis type="number" hide />
+            ) : (
+              <XAxis dataKey="name" axisLine={false} tickLine={false} />
+            )}
+            {isMobile ? (
+              <YAxis type="category" hide dataKey="name" />
+            ) : (
+              <YAxis type="number" axisLine={false} tick={true} />
+            )}
+            {/* <XAxis type="number" />
         <YAxis type="category" dataKey="name" /> */}
 
-        {!isMobile && <CartesianGrid vertical={false} strokeDashArray="7 5 2"/>}
-        <Bar
-          maxBarSize={isMobile?30:38}
-          shape={
-            <Rectangle radius={isMobile ? [0, 10, 10, 0] : [10, 10, 0, 0]} />
-          }
-          dataKey="data"
-          fill="#FF751D"
-          // shape={<TriangleBar />}
-          label={{ position: 'top', fill: '#52555F' }}
-        >
-          {diagramData.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={index===0||index % 3 === 0?"#FF751D":"#FFDAC0"} />
-          ))}
-          {isMobile &&<LabelList dataKey="name" position="center" fill="#52555F "/>}
-        </Bar>
-      </BarChart>
-    </ResponsiveContainer>}
-    </>
+            {!isMobile && (
+              <CartesianGrid vertical={false} strokeDashArray="7 5 2" />
+            )}
+            <Bar
+              maxBarSize={isMobile ? 30 : 38}
+              shape={
+                <Rectangle
+                  radius={isMobile ? [0, 10, 10, 0] : [10, 10, 0, 0]}
+                />
+              }
+              dataKey="data"
+              fill="#FF751D"
+              // shape={<TriangleBar />}
+              label={{ position: 'top', fill: '#52555F' }}
+            >
+              {diagramData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={index === 0 || index % 3 === 0 ? '#FF751D' : '#FFDAC0'}
+                />
+              ))}
+              {isMobile && (
+                <LabelList dataKey="name" position="center" fill="#52555F " />
+              )}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      )}
+    </div>
   );
 };
 export default Stats;
